48118b9dd1a437dc324e2e234a5b644c59db6a4d
Anna Li <annajxli@gmail.com>
2019-09-30 16:53:27 -0400

    Add checking fn to param set

--- ICtools.py
+++ ICtools.py
@@ -6,83 +6,132 @@ import os
 import tisgrabber as ic
 
 
-def setCameraParams(cam, params):
-    """ Set parameters of TIS camera object. Their code sucks.
+def _set_and_check(camFn, *pv):
+    """ Check that camera parameters are being set correctly.
+    The tisgrabber C wrapper returns a value on setting.
+    See their tisgrabber.h file for details on retvals.
 
-    :param cam: (TIC_CAM) instantiated camera object
-    :param params: (dict) dictionary of camera parameters to set
-    :return: nothing. camera parameters edited in place.
+    Args:
+        camFn: (fn) TIS_CAM function
+        *pv: args for function
+
+    Returns:
+        passes if param is set correctly.
+        raises RuntimeError if param is not set correctly.
+    """
+    retval = camFn(*pv)
+    if retval != 1:
+        raise RuntimeError('Error calling into API: %s, retval %d' % (str(camFn), retval))
+
+
+def setCameraDefaults(cam):
+    """
+
+    Args:
+        cam: (TIS_CAM) camera object
+
+    Returns:
+        nothing.
+
+    """
+    _set_and_check(cam.open, 'DMK 72BUC02 7610448')
+    _set_and_check(cam.SetVideoFormat, 'Y800 (640x480)')
+    _set_and_check(cam.SetFrameRate, 15.0)
+
+    print('Camera properties set successfully.')
+
+
+def setCameraParams(cam, params):
+    """ Set imaging parameters.
+
+    Args:
+        cam: (TIC_CAM) instantiated camera object
+        params: (dict) camera parameters
+
+    Returns:
+        nothing.
+
     """
     # honestly ugh
 
-    cam.SetPropertyValue('Brightness', 'Value', params['brightness'])
-    cam.SetPropertyValue('Contrast', 'Value', params['contrast'])
+    _set_and_check(cam.SetPropertyValue, 'Brightness', 'Value', params['brightness'])
+    _set_and_check(cam.SetPropertyValue, 'Contrast', 'Value', params['contrast'])
 
     if params['gain'] == 'auto':
-        cam.SetPropertySwitch('Gain', 'Auto', 1)
+        _set_and_check(cam.SetPropertySwitch, 'Gain', 'Auto', 1)
     else:
-        cam.SetPropertyValue('Gain', 'Value', params['gain'])
+        _set_and_check(cam.SetPropertySwitch, 'Gain', 'Auto', 0)
+        _set_and_check(cam.SetPropertyValue, 'Gain', 'Value', params['gain'])
 
-    # no auto - hard coded to avoid strobe probs
-    cam.SetPropertyValue('Exposure', 'Auto', 0)
+    if params['exposure'] == 'auto':
+        _set_and_check(cam.SetPropertySwitch, 'Exposure', 'Auto', 1)
+    else:
+        _set_and_check(cam.SetPropertySwitch, 'Exposure', 'Auto', 0)
+        _set_and_check(cam.SetPropertyAbsoluteValue, 'Exposure', 'Value', params['exposure'])
 
-    cam.SetPropertyAbsoluteValue('Exposure', 'Value', params['exposure'])
-    cam.SetPropertyValue('Exposure', 'Auto reference', params['exposureAutoRef'])
+    _set_and_check(cam.SetPropertyValue, 'Exposure', 'Auto Reference', params['exposureAutoRef'])
 
     if params['exposureAutoMax'] == 'auto':
-        cam.SetPropertySwitch('Exposure', 'Auto max value auto', 1)
+        raise ValueError('Trouble setting auto right now. Use a float value.')
+        # hypothesis: both auto exposure and exposure auto max auto are ID'd "Auto", which causes problems.
+        # _set_and_check(cam.SetPropertySwitch, 'Auto Max Value Auto', 'Enable', 1)
     else:
-        raise ValueError('only \'auto\' works right now.')
+        _set_and_check(cam.SetPropertyAbsoluteValue, 'Exposure', 'Auto Max Value', params['exposureAutoMax'])
 
     if params['highlightReduction']:
-        cam.SetPropertySwitch('Highlight reduction', 'Value', 1)
+        _set_and_check(cam.SetPropertySwitch, 'Highlight reduction', 'Enable', 1)
     else:
-        cam.SetPropertySwitch('Highlight reduction', 'Value', 0)
+        _set_and_check(cam.SetPropertySwitch, 'Highlight reduction', 'Enable', 0)
 
-    cam.SetPropertyValue('Sharpness', 'Value', params['sharpness'])
-    cam.SetPropertyValue('Gamma', 'Value', params['gamma'])
-    cam.SetPropertyValue('Denoise', 'Value', params['denoise'])
+    _set_and_check(cam.SetPropertyValue, 'Sharpness', 'Value', params['sharpness'])
+    _set_and_check(cam.SetPropertyValue, 'Sharpness', 'Value', params['sharpness'])
+    _set_and_check(cam.SetPropertyValue, 'Gamma', 'Value', params['gamma'])
+    _set_and_check(cam.SetPropertyValue, 'Denoise', 'Value', params['denoise'])
 
     if params['autoCenter']:
-        cam.SetPropertySwitch('Partial scan', 'Partial scan auto center', 1)
+        _set_and_check(cam.SetPropertySwitch, 'Partial scan', 'Auto-center', 1)
     else:
-        cam.SetPropertySwitch('Partial scan', 'Partial scan auto center', 0)
-        cam.SetPropertyValue('Partial scan', 'X offset', params['xOffset'])
-        cam.SetPropertyValue('Partial scan', 'Y offset', params['yOffset'])
+        _set_and_check(cam.SetPropertySwitch, 'Partial scan', 'Auto-center', 0)
+        _set_and_check(cam.SetPropertyValue, 'Partial scan', 'X Offset', params['xOffset'])
+        _set_and_check(cam.SetPropertyValue, 'Partial scan', 'Y Offset', params['yOffset'])
 
     if params['trigger']:
-        cam.SetPropertySwitch('Trigger', 'Enable', 1)
+        _set_and_check(cam.SetPropertySwitch, 'Trigger', 'Enable', 1)
     else:
-        cam.SetPropertySwitch('Trigger', 'Enable', 0)
+        _set_and_check(cam.SetPropertySwitch, 'Trigger', 'Enable', 0)
 
     if params['strobe']:
-        cam.SetPropertySwitch('Strobe', 'Enable', 1)
+        _set_and_check(cam.SetPropertySwitch, 'Strobe', 'Enable', 1)
     else:
-        cam.SetPropertySwitch('Strobe', 'Enable', 0)
+        _set_and_check(cam.SetPropertySwitch, 'Strobe', 'Enable', 0)
 
     if params['strobePolarity']:
-        cam.SetPropertySwitch('Strobe', 'Polarity', 1)
+        _set_and_check(cam.SetPropertySwitch, 'Strobe', 'Polarity', 1)
     else:
-        cam.SetPropertySwitch('Strobe', 'Polarity', 0)
+        _set_and_check(cam.SetPropertySwitch, 'Strobe', 'Polarity', 0)
 
     if params['toneMapping']:
-        cam.SetPropertySwitch('Tone mapping', 'Enable', 1)
+        _set_and_check(cam.SetPropertySwitch, 'Tone Mapping', 'Enable', 1)
     else:
-        cam.SetPropertySwitch('Tone mapping', 'Enable', 0)
+        _set_and_check(cam.SetPropertySwitch, 'Tone Mapping', 'Enable', 0)
+
+    print('Parameters set successfully.')
 
 
-def acquireStack(cam, acqLengthS, doStrobe, downscaleTuple, animal, outdir, saveTxt):
-    """ Take a timed image stack with the camera.
+def acquireStack(cam, acqLengthS, doStrobe, downscaleTuple, animal, outdir):
+    """   Get an image stack from the camera.
+     Args:
+        cam: (TIS_CAM) initialized camera object
+        acqLengthS: (int) acquisition length in seconds
+        doStrobe: (bool) whether or not to send strobe signal
+        downscaleTuple: (tuple) downscale factor in (z, x, y), e.g. (1, 2, 2) for 2x downscale
+        animal: (str) animal ID, used for output naming
+        outdir: (str) path to output directory
 
-    :param cam: (TIC_CAM) instantiated camera object
-    :param acqLengthS: (int) desired acquisition length in seconds
-    :param doStrobe: (bool) whether or not to send strobe signal, default True
-    :param downscaleTuple: (tuple) downscaling factors in (T, x, y), e.g. (1, 2, 2) for 2x downscale
-    :param animal: (str) animal ID
-    :param outdir: (str) folder to save stack to
-    :param saveTxt: (bool) whether or not to save params as a txt file
-    :return stack: (np array) acquired image stack
+    Returns:
+        stack: (np.ndarray) image stack in (z, x, y)
     """
+
     stack = []
     t_start = time.time()
     t_end = t_start + acqLengthS
@@ -90,8 +139,8 @@ def acquireStack(cam, acqLengthS, doStrobe, downscaleTuple, animal, outdir, save
     cam.StartLive(1)
 
     if doStrobe:
-        cam.SetPropertyValue('GPIO', 'GP Out', 1)
-        cam.SetPropertyValue('GPIO', 'Write', 1)
+        _set_and_check(cam.SetPropertyValue, 'GPIO', 'GP Out', 1)
+        _set_and_check(cam.PropertyOnePush, 'GPIO', 'Write')
 
     while time.time() < t_end:
         cam.SnapImage()
@@ -99,8 +148,9 @@ def acquireStack(cam, acqLengthS, doStrobe, downscaleTuple, animal, outdir, save
         stack.append(np.mean(im, axis=2))  # averaging to one frame
 
     if doStrobe:
-        cam.SetPropertyValue('GPIO', 'GP Out', 0)
-        cam.SetPropertyValue('GPIO', 'Write', 1)
+        _set_and_check(cam.SetPropertyValue, 'GPIO', 'GP Out', 0)
+        _set_and_check(cam.PropertyOnePush, 'GPIO', 'Write')
+
     cam.StopLive()
 
     stack = np.r_[stack]
@@ -111,81 +161,7 @@ def acquireStack(cam, acqLengthS, doStrobe, downscaleTuple, animal, outdir, save
     outfile = os.path.join(outdir, '{}{}.tif'.format(animal, timeStr))
     tfl.imsave(outfile, stack)
     nFrames = stack.shape[0]
-    
+
     print('Saved {} frames to {}'.format(nFrames, outfile))
 
-    if saveTxt:
-        params = getCameraParams(cam)
-        f = open(os.path.join(outdir,'{}{}.txt'.format(animal, timeStr)))
-        f.write(str(params))
-        f.close()
-
     return stack
-
-def getCameraParams(cam):
-    """ Create a dict of imaging params.
-
-    :param cam: camera object to collect params from
-    :return: (dict) of parameters
-    """
-    # automatic ones here
-    if cam.GetPropertySwitch('Gain', 'Auto') == 1:
-        gain = 'auto'
-    else:
-        gain = cam.GetPropertyValue('Gain', 'Value')
-
-    if cam.GetPropertySwitch('Exposure', 'Auto max value auto') == 1:
-        automax = 'auto'
-    else:
-        automax = cam.GetPropertyValue('Exposure', 'Auto max value')
-
-    if cam.GetPropertySwitch('Highlight reduction', 'Value') == 1:
-        highlightReduction = True
-    else:
-        highlightReduction = False
-
-    if cam.GetPropertySwitch('Partial scan', 'Partial scan auto center') == 1:
-        autoCenter = True
-    else:
-        autoCenter = False
-
-    if cam.GetPropertySwitch('Trigger', 'Enable') == 1:
-        trigger = True
-    else:
-        trigger = False
-
-    if cam.GetPropertySwitch('Strobe', 'Enable') == 1:
-        strobe = True
-    else:
-        strobe = False
-
-    if cam.GetPropertySwitch('Strobe', 'Polarity') == 1:
-        polarity = True
-    else:
-        polarity = False
-
-    if cam.GetPropertySwitch('Tone mapping', 'Enable') == 1:
-        toneMapping = True
-    else:
-        toneMapping = False
-
-    params = {
-        'brightness': cam.GetPropertyValue('Brightness', 'Value'),
-        'contrast': cam.GetPropertyValue('Gain', 'Value'),
-        'gain': gain,
-        'exposure': cam.GetPropertyAbsoluteValue('Exposure', 'Value'),
-        'exposure auto ref': cam.GetPropertyValue('Exposure', 'Auto reference'),
-        'exposure auto max': automax,
-        'highlight reduction': highlightReduction,
-        'sharpness': cam.GetPropertyValue('Sharpness', 'Value'),
-        'gamma': cam.GetPropertyValue('Gamma', 'Value'),
-        'denoise': cam.GetPropertyValue('Denoise', 'Value'),
-        'auto center': autoCenter,
-        'x offset': cam.GetPropertyValue('Partial scan', 'X offset'),
-        'y offset': cam.GetPropertyValue('Partial scan', 'Y offset'),
-        'trigger': trigger,
-        'strobe': strobe,
-        'strobe polarity': polarity,
-        'tone mapping': toneMapping
-             }
-    return params
